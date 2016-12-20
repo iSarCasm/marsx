d17c9a6e26e6e609fc5f627657e8d77f2bcb15a6
SarCasm <sarcasm008@gmail.com>
2016-12-21 00:34:37 +0200

    Video page

--- dev/js/main.js
+++ dev/js/main.js
@@ -10,6 +10,26 @@ $(document).ready(function() {
 		'speedOut'		:	200,
 		'overlayShow'	:	false
   });
+
+  $(".fancybox-video").click(function() {
+		$.fancybox({
+			'padding'		: 0,
+			'autoScale'		: false,
+			'transitionIn'	: 'none',
+			'transitionOut'	: 'none',
+			'title'			: this.title,
+			'width'			: 840,
+			'height'		: 485,
+			'href'			: this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+			'type'			: 'swf',
+			'swf'			: {
+  			'wmode'				: 'transparent',
+  			'allowfullscreen'	: 'true'
+			}
+		});
+
+		return false;
+	});
 });
 
 $('#responsive').orbit({bullets: true, fluid: true});
