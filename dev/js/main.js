ff9711f7cceec2b008554f54e546abff38cc6f9d
SarCasm <sarcasm008@gmail.com>
2016-12-21 00:23:41 +0200

    gallery page worked out

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
   Tipped.create('.box');
 
   $('#maintitle').addClass('wide');
+
+  $('.fancybox-image').fancybox({
+    'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600,
+		'speedOut'		:	200,
+		'overlayShow'	:	false
+  });
 });
 
 $('#responsive').orbit({bullets: true, fluid: true});
